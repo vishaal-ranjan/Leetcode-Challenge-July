64f9026f11cf11f057fc23b9f7ed882b0af2e940
Vishaal Ranjan <vishaal811@gmail.com>
2020-07-11 11:52:02 -0400

    Dy 10

---
+++ 10_Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Node') -> 'Node':
+        if not head:
+            return head
+
+        # pseudo head to ensure the `prev` pointer is never none
+        pseudoHead = Node(None, None, head, None)
+        self.flatten_dfs(pseudoHead, head)
+
+        # detach the pseudo head from the real head
+        pseudoHead.next.prev = None
+        return pseudoHead.next
+
+
+    def flatten_dfs(self, prev, curr):
+        """ return the tail of the flatten list """
+        if not curr:
+            return prev
+
+        curr.prev = prev
+        prev.next = curr
+
+        # the curr.next would be tempered in the recursive function
+        tempNext = curr.next
+        tail = self.flatten_dfs(curr, curr.child)
+        curr.child = None
+        return self.flatten_dfs(tail, tempNext)
